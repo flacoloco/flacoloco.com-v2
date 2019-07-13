8372cd81b2b5a630bf42a8bca88805e255d13179
Alejandro Vásquez <alejandro.vasquez@bigfinite.com>
2019-07-13 21:39:40 +0200

    main behaviour ok

--- src/Terminal/Terminal.jsx
+++ src/Terminal/Terminal.jsx
@@ -1,18 +1,29 @@
 import React, { memo, useRef, useState } from 'react'
 
-import { Container, Console, Header } from './Terminal.styles'
+import {
+  Container,
+  Console,
+  ConsoleWrapper,
+  ExternalLink,
+  Header,
+  Input,
+} from './Terminal.styles'
 
 import data from './data.json'
 
+const randomIndex = max => Math.floor(Math.random() * max)
+
 const getRandomError = () => {
-  const index = 3
+  const index = randomIndex(data.errors.length)
   return { msg: data.errors[index] }
 }
 
 const getRandomSuccess = () => {
-  const index = 1
+  const index = randomIndex(data.success.length)
   return { msg: data.success[index] }
 }
+
+const Path = () => <span>flaco ></span>
 
 function Terminal() {
   const [resp, setResp] = useState([])
@@ -21,35 +32,50 @@ function Terminal() {
 
   const findCard = () => {
     const respNew = [...resp]
-    const found = data.cards.filter(c => c.tags.includes(inputRef.current.value))
+    const { current: { value } } = inputRef
+    respNew.push({ path: true, msg: value })
+    const found = data.cards.filter(c => c.tags.includes(value))
     if (!found.length) {
-      console.log('no results')
-      // get random error
       const error = getRandomError()
-      console.log('error', error)
       respNew.push(error)
     } else {
       const success = getRandomSuccess()
       respNew.push(success)
       found.forEach(f => respNew.push(f))
     }
-    console.log('respNew', respNew)
     setResp(respNew)
   }
 
   const keyDownHandler = (e) => {
-    if (e.key === 'Enter') findCard()
+    if (e.key === 'Enter') {
+      findCard()
+      inputRef.current.value = ''
+    }
   }
-
+  console.log('resp', resp);
   return (
     <Container>
-      <Header>header</Header>
-      <Console>
-        {resp.map((r, i) => (
-          <li key={i}>{r.msg}</li>
-        ))}
-      </Console>
-      <input ref={inputRef} type="text" onKeyDown={keyDownHandler} />
+      <ConsoleWrapper>
+        <Header>header</Header>
+        <Console>
+          {resp.map((r, i) => (
+            <li key={i}>
+              {r.path && <Path />}
+              {r.msg}
+              {r.link
+              && (
+                <ExternalLink>
+                  <a href={r.link} rel="noopener noreferrer" target="_blank">{r.link}</a>
+                </ExternalLink>
+              )}
+            </li>
+          ))}
+        </Console>
+        <Input>
+          <Path />
+          <input ref={inputRef} type="text" onKeyDown={keyDownHandler} />
+        </Input>
+      </ConsoleWrapper>
     </Container>
   )
 }
--- src/Terminal/Terminal.styles.js
+++ src/Terminal/Terminal.styles.js
@@ -1,18 +1,39 @@
 import styled from 'styled-components'
 
+const darkBlue = '#0D2B36'
+const blue = '#438BCC'
+const red = '#EE6A5F'
+const green = '#62C554'
+const yellow = '#F7C251'
+const lightGray = '#D7D7D6'
+const gray = '#95A0A1'
 
 export const Container = styled.div`
   display: flex;
-  flex-direction: column;
-  width: 400px;
+  justify-content: center;
+  align-items: center;
+  background-color: #000;
+`
+export const ConsoleWrapper = styled.div`
+  width: 640px;
+  height: 180px;
+  background-color: ${darkBlue};
+  overflow: auto;
+`
+export const Console = styled.ul`
+  color: ${gray};
+  height: auto;
 `
 
-export const Console = styled.ul`
-  color: #fff;
-  height: auto;
-  background-color: #0D2B36;
+export const ExternalLink = styled.div`
+  & a:link {
+    color: ${lightGray}
+  }
 `
 
 export const Header = styled.div`
-  background-color: #D7D7D6
+  background-color: ${lightGray};
+`
+export const Input = styled.div`
+  display: flex;
 `

34f5105092fb3fa334759a41c0ff2b6530774e7c
Alejandro Vásquez <alejandro.vasquez@bigfinite.com>
2019-07-14 02:15:15 +0200

    css reset added

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,8 @@
-import React, { Fragment } from 'react'
-import styled from 'styled-components'
-
-import potato from './potato.gif'
-
-const Hello = styled.p`
-  background-color: yellow;
-  font-size: 20px;
-  padding: 20px;
-`
+import React from 'react'
+import Terminal from './Terminal/Terminal'
 
 const App = () => (
-  <Fragment>
-    <Hello>React presents... the amazing dancing potato!</Hello>
-    <img alt="dancing potato" src={potato} />
-  </Fragment>
+  <Terminal />
 )
 
 export default App
